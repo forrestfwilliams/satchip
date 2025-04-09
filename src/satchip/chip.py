93bb2dc949fba9fd6bf82ef5a87a9f30c0c1f0d3
Forrest Williams <ffwilliams2@alaska.edu>
2025-04-09 08:30:44 -0500

    download s2 data

--- src/satchip/chip.py
+++ src/satchip/chip.py
@@ -22,7 +22,7 @@ def chip_labels(label_path: Path, date: datetime, output_dir: Path) -> Path:
     bbox = utils.get_epsg4326_bbox(label.rio.bounds(), label.rio.crs.to_epsg())
     tm_grid = TerraMindGrid(latitude_range=(bbox[1], bbox[3]), longitude_range=(bbox[0], bbox[2]))
     chips = {}
-    for tm_chip in tqdm(tm_grid.terra_mind_chips[2000:2500]):
+    for tm_chip in tqdm(tm_grid.terra_mind_chips):
         chip = label.rio.reproject(
             dst_crs=f'EPSG:{tm_chip.epsg}',
             resampling=rio.enums.Resampling(1),
--- src/satchip/chip_sentinel2.py
+++ src/satchip/chip_sentinel2.py
@@ -1,25 +1,85 @@
 import argparse
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
 
 import numpy as np
+import rioxarray
+import shapely
 import xarray as xr
-import zarr
+from pystac_client import Client
 
 from satchip import utils
-from satchip.major_tom_grid import MajorTomGrid
+from satchip.terra_mind_grid import TerraMindChip, TerraMindGrid
 
 
-def get_data(sample: str, date: datetime) -> np.ndarray:
-    return None
+def create_template_da(chip: TerraMindChip) -> xr.DataArray:
+    x = np.arange(chip.nrow) * chip.gdal_transform[1] + chip.gdal_transform[0] + chip.gdal_transform[1] / 2
+    y = np.arange(chip.ncol) * chip.gdal_transform[5] + chip.gdal_transform[3] + chip.gdal_transform[5] / 2
+    template = xr.DataArray(np.zeros((chip.ncol, chip.nrow)), dims=('y', 'x'), coords={'y': y, 'x': x})
+    template.rio.write_crs(f'EPSG:{chip.epsg}', inplace=True)
+    template.rio.write_transform(chip.rio_transform, inplace=True)
+    return template
+
+
+def get_s2l2a_data(chip: TerraMindChip, date: datetime) -> xr.DataArray:
+    """Returns XArray DataArray of Sentinel-2 L2A image for the given bounds and
+    closest collection after date.
+
+    If multiple images are available, the one with the most coverage is returned.
+    """
+    date_end = date + timedelta(weeks=1)
+    date_range = f'{datetime.strftime(date, "%Y-%m-%d")}/{datetime.strftime(date_end, "%Y-%m-%d")}'
+    roi = shapely.box(*chip.bounds)
+    client = Client.open('https://earth-search.aws.element84.com/v1')
+    search = client.search(
+        collections=['sentinel-2-l2a'],
+        intersects=roi,
+        datetime=date_range,
+        max_items=50,
+    )
+    items = list(search.item_collection())
+    items.sort(key=lambda x: x.datetime)
+    coverage = []
+    for item in search.item_collection():
+        image_footprint = shapely.geometry.shape(item.geometry)
+        intersection = roi.intersection(image_footprint)
+        coverage.append(intersection.area / roi.area)
+    item = items[coverage.index(max(coverage))]
+    bands = {
+        'B01': 'coastal',
+        'B02': 'blue',
+        'B03': 'green',
+        'B04': 'red',
+        'B05': 'rededge1',
+        'B06': 'rededge2',
+        'B07': 'rededge3',
+        'B08': 'nir',
+        'B8A': 'nir08',
+        'B09': 'nir09',
+        'B11': 'swir16',
+        'B12': 'swir22',
+    }
+    roi_buffered = roi.buffer(0.1)
+    das = []
+    template = create_template_da(chip)
+    for band in bands:
+        href = item.assets[bands[band]].href
+        da = rioxarray.open_rasterio(href).rio.clip_box(*roi_buffered.bounds, crs='EPSG:4326')
+        da['band'] = [band]
+        da_reproj = da.rio.reproject_match(template)
+        das.append(da_reproj)
+    dataarray = xr.concat(das, dim='band')
+    return dataarray
 
 
 def chip_sentinel2(label_path: str, output_dir: Path) -> Path:
     labels = utils.load_chip(label_path)
-    major_tom_chips = list(set([x[0:9] for x in labels.coords['sample'].data]))
     date = labels.time.data[0].astype('M8[ms]').astype(datetime)
-    data = get_data(major_tom_chips[0], date)
-    # get sample name
+    bounds = labels.attrs['bounds']
+    grid = TerraMindGrid([bounds[1], bounds[3]], [bounds[0], bounds[2]])
+    data_chips = []
+    for chip in grid.terra_mind_chips:
+        data_chips.append(get_s2l2a_data(chip, date))
     return labels
 
 
