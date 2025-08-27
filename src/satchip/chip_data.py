742a1ab17d863482f167252a45b068c2805ef333
Forrest Williams <ffwilliams2@alaska.edu>
2025-08-27 14:45:19 -0500

    add working HLS

--- src/satchip/chip_data.py
+++ src/satchip/chip_data.py
@@ -8,15 +8,16 @@ from tqdm import tqdm
 
 import satchip
 from satchip import utils
+from satchip.chip_hls import get_hls_data
 from satchip.chip_sentinel1rtc import get_s1rtc_data
 from satchip.chip_sentinel2 import get_s2l2a_data
 from satchip.terra_mind_grid import TerraMindGrid
 
 
-GET_DATA_FNS = {'S2L2A': get_s2l2a_data, 'S1RTC': get_s1rtc_data}
+GET_DATA_FNS = {'S2L2A': get_s2l2a_data, 'S1RTC': get_s1rtc_data, 'HLS': get_hls_data}
 
 
-def chip_data(label_path: str, platform: str, output_dir: Path) -> Path:
+def chip_data(label_path: Path, platform: str, output_dir: Path, scratch_dir: Path | None) -> xr.Dataset:
     get_data_fn = GET_DATA_FNS[platform]
     labels = utils.load_chip(label_path)
     date = labels.time.data[0].astype('M8[ms]').astype(datetime)
@@ -25,10 +26,14 @@ def chip_data(label_path: str, platform: str, output_dir: Path) -> Path:
     terra_mind_chips = [c for c in grid.terra_mind_chips if c.name in list(labels.sample.data)]
 
     data_chips = []
-    with TemporaryDirectory() as scratch_dir:
-        scratch_dir = Path(scratch_dir)
+    if scratch_dir is not None:
         for chip in tqdm(terra_mind_chips):
             data_chips.append(get_data_fn(chip, date, scratch_dir))
+    else:
+        with TemporaryDirectory() as tmp_dir:
+            scratch_dir = Path(tmp_dir)
+            for chip in tqdm(terra_mind_chips):
+                data_chips.append(get_data_fn(chip, date, scratch_dir))
 
     attrs = {'date_created': date.isoformat(), 'satchip_version': satchip.__version__, 'bounds': labels.attrs['bounds']}
     dataset = xr.Dataset(attrs=attrs)
@@ -43,16 +48,15 @@ def chip_data(label_path: str, platform: str, output_dir: Path) -> Path:
 
 def main() -> None:
     parser = argparse.ArgumentParser(description='Chip a label image')
-    parser.add_argument('labelpath', type=str, help='Path to the label image')
-    parser.add_argument('platform', choices=['S2L2A', 'S1RTC'], type=str, help='Dataset to create chips for')
-    parser.add_argument('--outdir', default='.', type=str, help='Output directory for the chips')
+    parser.add_argument('labelpath', type=Path, help='Path to the label image')
+    parser.add_argument('platform', choices=['S2L2A', 'S1RTC', 'HLS'], type=str, help='Dataset to create chips for')
+    parser.add_argument('--outdir', default='.', type=Path, help='Output directory for the chips')
+    parser.add_argument(
+        '--scratchdir', default=None, type=Path, help='Output directory for scratch files if you want to keep them'
+    )
     args = parser.parse_args()
-
-    args.labelpath = Path(args.labelpath)
     args.platform = args.platform.upper()
-    args.outdir = Path(args.outdir)
-
-    chip_data(args.labelpath, args.platform, args.outdir)
+    chip_data(args.labelpath, args.platform, args.outdir, args.scratchdir)
 
 
 if __name__ == '__main__':
